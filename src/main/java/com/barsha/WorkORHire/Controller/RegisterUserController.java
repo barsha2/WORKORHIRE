214c16524620d29b793151f066122ede99348290
unknown <barshapodder2499@gmail.com>
2024-03-27 22:49:40 +0530

    Project Started 27-03-2024

---
+++ src/main/java/com/barsha/WorkORHire/Controller/RegisterUserController.java
@@ -0,0 +1,10 @@
+package com.barsha.WorkORHire.Controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/RegisterUser")
+public class RegisterUserController {
+    
+}
